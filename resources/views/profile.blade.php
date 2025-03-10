2dd1a211c4b9bcafb5208926efeb8172930d0965
chasamelia <ameliachasanah320@gmail.com>
2025-03-10 12:37:13 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Profil Saya</h1>
+                </div>
+                <div class="card-body">
+                    <img src="giselleaespa.jpg" alt="Profile Picture" width="150">
+                    <h3> Amelia Chasanah</h3>
+                    <p>Email: ameliachasanah320@gmail.com</p>
+                    <p>Bio: Siswa SMK N 1 Sayung</p>
+                    <h5>Skills: </h5>
+                    <ul>
+                        <li>Manajemen Waktu</li>
+                        <li>Kerja Tim/Kelompok</li>
+                        <li>Kreativitas</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
